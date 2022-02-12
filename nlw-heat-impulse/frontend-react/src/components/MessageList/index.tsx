b03702b315764a20858c5b129bdbe43ba204ed44
Nanda <nana.script.js@gmail.com>
2022-02-12 17:11:19 -0300

    Add and style MessageList
    NLW Heat: Impulse - Stage 02

--- nlw-heat-impulse/frontend-react/src/components/MessageList/index.tsx
+++ nlw-heat-impulse/frontend-react/src/components/MessageList/index.tsx
@@ -1,7 +1,63 @@
+import styles from './styles.module.scss'
+
+import logo from '../../assets/logo.svg'
+
 export function MessageList() {
     return (
         <>
-            <h1>MessageList</h1>
+            <div className={styles.messageListWrapper}>
+                <img src={logo} alt='DOWhile 2021' />
+
+                <ul className={styles.messageList}>
+                    <li className={styles.message}>
+                        <p className={styles.messageContent}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Debitis quod, numquam eaque placeat neque exercitationem
+                            officia nisi velit modi consequuntur. Dolorum!
+                        </p>
+
+                        <div className={styles.messageUser}>
+                            <div className={styles.userPic}>
+                                <img src='https://github.com/nanda-st.png' alt='Nanda Santos' />
+                            </div>
+
+                            <span>Nanda Santos</span>
+                        </div>
+                    </li>
+
+                    <li className={styles.message}>
+                        <p className={styles.messageContent}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Debitis quod, numquam eaque placeat neque exercitationem
+                            officia nisi velit modi consequuntur. Dolorum!
+                        </p>
+
+                        <div className={styles.messageUser}>
+                            <div className={styles.userPic}>
+                                <img src='https://github.com/nanda-st.png' alt='Nanda Santos' />
+                            </div>
+
+                            <span>Nanda Santos</span>
+                        </div>
+                    </li>
+
+                    <li className={styles.message}>
+                        <p className={styles.messageContent}>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Debitis quod, numquam eaque placeat neque exercitationem
+                            officia nisi velit modi consequuntur. Dolorum!
+                        </p>
+
+                        <div className={styles.messageUser}>
+                            <div className={styles.userPic}>
+                                <img src='https://github.com/nanda-st.png' alt='Nanda Santos' />
+                            </div>
+
+                            <span>Nanda Santos</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
